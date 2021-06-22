b5753d9071c48ab7aa597bc06d3b6f57c85b9359
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-06-22 16:54:41 +0200

    udpates

--- 2020/day3/main.py
+++ 2020/day3/main.py
@@ -2,28 +2,10 @@ step = 3
 
 
 def get_char(s, i):
-    if i * step < len(s):
-        # print(step * i, i, s[step * i])
-        return s[step * i]
-    else:
-        p = step * i
-        z = len(s) - 1
-        r = p % z
-
-        if r % 2 == 0:
-            if r != 0:
-                # print('d: ', r - 1, i, s[r - 1])
-                return s[r - 1]
-            else:
-                # print('a: ', r + 1, i, s[r + 1])
-                return s[r + 1]
-        else:
-            if r <= len(s) / 2:
-                # print('t: ', r, i, s[r])
-                return s[r]
-            else:
-                # print('r: ', r - 2, i, s[r - 2])
-                return s[r - 2]
+    p = step * i
+    z = len(s)
+    r = p % z
+    return s[r]
 
 
 def load_data():
@@ -33,14 +15,13 @@ def load_data():
 
 
 def validate():
-    squares = 0
+    squares = -1
     trees = 0
     for c in load_data():
         if c == '.':
             squares += 1
         elif c == '#':
             trees += 1
-    print('squares: ', squares, 'trees: ', trees)
 
 
 validate()
