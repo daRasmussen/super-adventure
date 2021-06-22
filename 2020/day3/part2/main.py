998632f51f6d2dba0b3dbbf72a2a192e3795f8c9
daniel.rasmussen@drgeo.se <daniel.rasmussen@drgeo.se>
2021-06-22 19:41:49 +0200

    updates

---
+++ 2020/day3/part2/main.py
@@ -0,0 +1,27 @@
+right = 3
+
+
+def get_char(s, i):
+    p = right * i
+    z = len(s)
+    r = p % z
+    return s[r]
+
+
+def load_data():
+    with open("map.txt", "r") as d:
+        for index, line in enumerate(d):
+            yield get_char(line.strip(), index)
+
+
+def validate():
+    squares = -1
+    trees = 0
+    for c in load_data():
+        if c == '.':
+            squares += 1
+        elif c == '#':
+            trees += 1
+
+
+validate()
