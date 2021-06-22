eb4320822d30a604dc0b6cc63b4859483097e168
daniel.rasmussen@drgeo.se <daniel.rasmussen@drgeo.se>
2021-06-22 10:54:36 +0200

    updates

--- 2020/day3/main.py
+++ 2020/day3/main.py
@@ -2,19 +2,33 @@ import math
 
 step = 3
 
-
 def get_char(s, i):
     if i * step < len(s):
-        print(step * i, i)
+        print(step * i, i, s[step * i])
         return s[step * i]
     else:
         p = step * i
         q = math.floor(p / len(s))
         z = len(s) - 1
-        print(p % z, i)
-        return s[p % z]
+        r = p % z
+        c = math.floor(i / step)
 
+        # print(r, i, s[r], c)
+        # return s[r]
 
+        if r % 2 == 0:
+            print(r - 1, i, s[r - 1], c)
+            return s[r - 1]
+        else:
+            print(r, i, s[r], c)
+            return s[r]
+
+# calc index
+# calc x and y pos ...
+# pos x = row * step
+
+# get_char takes a index
+# returns s[i]
 def load_data():
     with open("test.txt", "r") as d:
         for index, line in enumerate(d):
