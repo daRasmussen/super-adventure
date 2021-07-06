8e327882048f0361636c9add63ec766e29a62d4d
echo <echo>
2021-07-06 10:05:02 +0200

    updates

---
+++ 2020/day4/python/part1/main.py
@@ -0,0 +1,20 @@
+"""
+This is main solution for day4 part 1 of
+Advent of Code 2020
+"""
+
+"""
+Validate passport data, if it contains right number of keys.
+Only key that could be missing is CID - Country Id
+"""
+
+file_name = "test.txt"
+
+
+def open_file(file_name):
+    with open(file_name, "r") as d:
+        yield d.readlines()
+
+
+def __init__(self):
+    print('HELLO WORLD')
