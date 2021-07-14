45a707d1a4e17f64bbbab1405882f94ff529e909
echo <echo>
2021-07-14 09:33:42 +0200

    updates

--- 2020/day4/python/part1/main.py
+++ 2020/day4/python/part1/main.py
@@ -8,13 +8,36 @@ Validate passport data, if it contains right number of keys.
 Only key that could be missing is CID - Country Id
 """
 
+import uuid
+
 file_name = "test.txt"
 
+valid_keys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid']
+pps = {}
+passport = []
 
-def open_file(file_name):
-    with open(file_name, "r") as d:
-        yield d.readlines()
+
+def check_passport(ud, passport_as_list):
+    """ Takes a list of passport keys and values, and checks each key if that key is in valid_keys. Returns tuple
+     with uuid is valid true or false."""
+    is_valid = False
+    return ud, is_valid
 
 
-def __init__(self):
-    print('HELLO WORLD')
+with open(file_name, "r") as d:
+    print(check_passport('test', []))
+    for l in d:
+        line = l.strip().split()
+        if not len(line) == 0:
+            passport += line
+        else:
+            pps[uuid.uuid4()] = passport
+            passport = []
+    if not len(passport) == 0:
+        pps[uuid.uuid4()] = passport
+        passport = []
+    res = {}
+    for uuid, pp_as_list in pps.items():
+        res += check_passport(uuid, pp_as_list)
+        print(res)
+    print(len(res))
