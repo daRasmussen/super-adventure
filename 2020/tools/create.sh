088658534a38244aa8645833b6667aa35eb671d5
echo <echo>
2021-08-27 15:47:30 +0200

    updates

---
+++ 2020/tools/create.sh
@@ -0,0 +1,11 @@
+mkdir python
+mkdir python/part1
+mkdir python/part2
+
+cp main_template.py python/part1/main.py
+touch python/part1/test.txt
+touch python/part1/data.txt
+
+cp main_template.py python/part2/main.py
+touch python/part2/test.txt
+touch python/part2/data.txt
