946fc12eeb83391bfbcc2a28a7c914fd28df1462
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-11-26 19:38:57 +0100

    resolves day7 part 1

--- 2020/day7/python/part1/main.py
+++ 2020/day7/python/part1/main.py
@@ -3,33 +3,22 @@ from collections import deque
 
 target = 'shiny gold'
 
-db = {
-    "no other bags": []
-}
+rules = {}
 
-
-def hasPath(graph, src, dst):
-    que = deque([src])
-    while len(que) > 0:
-        current = que.popleft()
-        if current == dst:
-            return True
-        for neighbour in graph[current]:
-            que.append(neighbour)
-    return False
-
-
-def bfs(graph, start):
-    visited, queue = set(), deque([start])
-    while queue:
-        node = queue.popleft()
-        if node not in visited:
-            print("Now visiting", node)
-        visited.add(node)
-        for neighbor in graph[node]:
-            queue.append(neighbor)
-    return visited
-
+def find():
+    total = 0
+    for rule in rules:
+        bags = [rule]
+        while len(bags) > 0:
+            if bags[0] in rules:
+                for newbag in rules[bags[0]]:
+                    if newbag[1] not in bags:
+                        bags.append(newbag[1])
+            del bags[0]
+            if target in bags:
+                total += 1
+                break
+    print(total)
 
 def create_graph(lines):
     for line in lines:
@@ -39,21 +28,15 @@ def create_graph(lines):
         key = parent.split(" bags")[:1]
         tmp = [x.strip() for x in children.split(", ")]
         tmp = [re.match("(\w+).(\w+).(\w+)", x).groups() for x in tmp]
-        res = []
-        for item in tmp:
-            if not item[0].isnumeric():
-                res.append(f"{item[0]} {item[1]} {item[2]}")
-            else:
-                res.append(f"{item[1]} {item[2]}")
-        db[str(key[0])] = res
+        tmp = [[int(x[0])  if x[0].isnumeric() else 0, f"{x[1]} {x[2]}" ] for x in tmp]
+        rules[str(key[0])] = tmp
 
 
 def main():
-    with open("test.txt", "r") as data:
+    with open("data.txt", "r") as data:
         create_graph(data.readlines())
 
 
 if __name__ == '__main__':
     main()
-    bfs(db, target)
-    print(hasPath(db, target, target))
+    find()
