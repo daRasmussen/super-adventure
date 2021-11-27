a5a71a9dbb6275267e234436907dfd7bd9db01f7
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-11-27 13:00:29 +0100

    fixes tests day7 part 2

--- 2020/day7/python/part2/init.py
+++ 2020/day7/python/part2/init.py
@@ -1,6 +1,6 @@
 import re
 import math
-
+import random
 
 def get_count(child):
     if child == ' no other bags.':
@@ -17,23 +17,27 @@ def get_parent(child):
 if __name__ == '__main__':
     with open("test.txt", "r") as data:
         lines = data.readlines()
+        random.shuffle(lines)
         target = "shiny gold"
         rules = {}
         """ Create rules. """
-        for index, line in enumerate(lines):
+        for line in lines:
             parent, child = line.strip().split("bags contain")
             rules[parent.strip()] = [child]
 
-        c = 0 # TODO: ADD COUNT !
+        c = 1
+        cc = []
         """ Search """
         while target != 'no other bags.':
             for rule in rules:
                 if rule == target:
                     child = rules[rule][0]
                     target = get_parent(child)
-                    print(target, child)
-                    #print(target != 'other bags.')
-                #print(target)
+                    count = get_count(child)
+                    if count != 0:
+                        c = c * count
+                        cc.append(c)
+                        print(target, child, c, cc, sum(cc))
 
 
 
