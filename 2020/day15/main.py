00c02e6cec21738ed720354128b004b35d38bded
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-11-30 21:48:34 +0100

    fixes part 2

--- 2020/day15/main.py
+++ 2020/day15/main.py
@@ -3,12 +3,12 @@ with open('data.txt') as file:
     data = data[0].split(',')
     data = [int(x) for x in data]
 
-def play_memory_game():
+def play_memory_game(size):
     mem = {}
     for i in range(len(data)-1):
         num = data[i]
         mem[num] = i
-    for i in range(len(data)-1, 2019):
+    for i in range(len(data)-1, size-1):
         num = data[i]
         if num not in mem:
             data.append(0)
@@ -20,4 +20,6 @@ def play_memory_game():
             mem[num] = i
     return data[-1]
 
-print(play_memory_game())
+print(play_memory_game(2020))
+print(play_memory_game(30000000))
+
