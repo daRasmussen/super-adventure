3fcf5fc30d6b210ff67d4010ef62c21109665391
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-11-30 16:08:35 +0100

    fixes part 2

--- 2020/day12/main.py
+++ 2020/day12/main.py
@@ -27,4 +27,35 @@ def get_displacement():
 
 print(get_displacement())
 
+# part 2
+def get_distance_with_waypoint():
+    x, y = 0, 0
+    i, j = 10, 1
+    for line in data:
+        cmd, value = line[0], int(line[1:])
+        if cmd == 'N':
+            j += value
+        elif cmd == 'S':
+            j -= value
+        elif cmd == 'E':
+            i += value
+        elif cmd == 'W':
+            i -= value
+        elif cmd == 'R':
+            while value > 0:
+                i1 = j
+                j1 = -1 * i
+                i, j = i1, j1
+                value -= 90
+        elif cmd == 'L':
+            while value > 0:
+                i1 = -1 * j
+                j1 = i
+                i, j = i1, j1
+                value -= 90
+        elif cmd == 'F':
+            x += value * i
+            y += value * j
+    return abs(x) + abs(y)
 
+print(get_distance_with_waypoint()) 
