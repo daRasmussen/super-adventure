f02c0284b295e249777e0706729e74469183a622
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-11-30 17:38:43 +0100

    fixes part 2

--- 2020/day14/main.py
+++ 2020/day14/main.py
@@ -53,3 +53,45 @@ def get_mem_total():
     return total
 
 print(get_mem_total())
+
+# part 2
+
+def get_addresses(s):
+    if 'X' not in s:
+        return s
+    addresses = ''
+    xi = s.index('X')
+    s0 = s[:xi] + '0' + s[xi+1:]
+    s1 = s[:xi] + '1' + s[xi+1:]
+    addresses += get_addresses(s0) + ','
+    addresses += get_addresses(s1)
+    return addresses
+
+def address_mask_total():
+    mem = {}
+    mask = ''
+    for line in data:
+        line = line.split(' = ')
+        if 'mask' in line[0]:
+            mask = line[1]
+        else:
+            value = int(line[1])
+            location = int(line[0][4:-1])
+            location_binary = list(to_binay(location))
+            # apply the mask
+            for i in range(len(mask)):
+                if mask[i] != '0':
+                    location_binary[i] = mask[i]
+            masked_location = ''
+            masked_location = masked_location.join(location_binary)
+            addresses = get_addresses(masked_location).split(',')
+
+            for address in addresses:
+                mem[address] = value
+
+    total = 0
+    for i in mem.values():
+        total += i
+    return total
+
+print(address_mask_total())
