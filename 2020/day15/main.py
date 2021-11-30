144e40639cc5ea9feb3a8e58231ab6bd23d7bf2c
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-11-30 21:44:04 +0100

    fixes part 1

---
+++ 2020/day15/main.py
@@ -0,0 +1,23 @@
+with open('data.txt') as file:
+    data = file.readlines()
+    data = data[0].split(',')
+    data = [int(x) for x in data]
+
+def play_memory_game():
+    mem = {}
+    for i in range(len(data)-1):
+        num = data[i]
+        mem[num] = i
+    for i in range(len(data)-1, 2019):
+        num = data[i]
+        if num not in mem:
+            data.append(0)
+            mem[num] = i
+        else:
+            j = mem[num]
+            new_num = i - j
+            data.append(new_num)
+            mem[num] = i
+    return data[-1]
+
+print(play_memory_game())
