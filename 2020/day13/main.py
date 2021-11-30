93b3234f4b50ca6e688e3fd83e4c6eaae8413696
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-11-30 16:51:23 +0100

    fixes part 2

--- 2020/day13/main.py
+++ 2020/day13/main.py
@@ -22,3 +22,40 @@ def get_id_minutes():
 
 print(get_id_minutes())
 
+# part 2 
+# Chinese remainder theorem 
+# bus k at index i departs at a time t+1
+# t+i % k == 0
+# t % k == -i
+# t % k == k-i
+# index = (k - (i%k)) % k
+
+def mod_inverse(a, n):
+    # find some x such that (a*x) % n == 1
+    a = a % n
+    if n == 1:
+        return 1
+    for x in range(1, n):
+        if ((a * x) % n == 1):
+            return x
+
+def get_earliest_time():
+    ids = []
+    full_product = 1
+    for i in range(len(data[1])):
+        item = data[1][i]
+        if item != 'x':
+            k = int(item)
+            i = i % k
+            ids.append(((k-i)%k,k))
+            full_product *= k
+    total = 0
+    for i,k in ids:
+        partial_product = full_product // k
+        inverse = mod_inverse(partial_product,k)
+        assert (inverse * partial_product) % k == 1
+        term = inverse * partial_product * i
+        total += term
+    return total % full_product
+
+print(get_earliest_time())
