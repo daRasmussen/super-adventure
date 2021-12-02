224ffbf7afe2a9c8cde552cbda75b23f7df7ad50
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-02 17:16:38 +0100

    part 2 wip

--- 2019/day2/main.py
+++ 2019/day2/main.py
@@ -3,10 +3,9 @@ with open('data.txt') as f:
     d = [x.strip() for x in d]
     li = d[0].split(",")
     li = [int(x) for x in li]
-    #li = [1,9,10,3,2,3,11,0,99,30,40,50]
     li[1] = 12
     li[2] = 2
-t = []
+t= []
 for l in li:
     if len(t) != 4:
         t.append(l)
@@ -20,4 +19,39 @@ for l in li:
                 break
     else:
         t = [l]
-print(li)
+print(li[0])
+
+# part 2
+with open('data.txt') as f:
+    d = f.readlines()
+    d = [x.strip() for x in d]
+    li = d[0].split(",")
+    li = [int(x) for x in li]
+noun, verb = [0, 1]
+li[1] = noun
+li[2] = verb
+t = 19690720
+p = [] 
+while li[0] != t:
+    noun, verb = li[0], li[1]
+    if noun == verb:
+        noun += 1
+    elif noun > verb:
+        verb += 1
+    elif noun < verb:
+        noun += 1
+    for a in li:
+        if len(p) != 4:
+            p.append(a)
+            if len(p) == 4:
+                c, a, b, o = p[0], p[1], p[2], p[3]
+                print('li:',len(li),'c: ', c,'a: ',a,'b: ', 'o: ', o)
+                if c == 1:
+                    li[o] = li[a] + li[b]
+                if c == 2:
+                    li[o] = li[a] * li[b]
+                if c == 99:
+                    break
+        else:
+            p = [a]
+    print(100 * li[0] + li[1])
