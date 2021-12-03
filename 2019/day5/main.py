977d781c5636317bf57cabada2c241bffbad2454
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-03 03:00:37 +0100

    fixes part 1

--- 2019/day5/main.py
+++ 2019/day5/main.py
@@ -1,74 +1,58 @@
-data = []
-with open("data.txt") as f:
-    data = [int(x) for x in f.read().split(",")]
+def read_intcode(file):
+    with open(file) as f:
+        return list(map(int, f.read().split(",")))
 
-def get_parameter_modes(op):
-    # the first parameter's mode is in the hundrets digit.
-    mode1 = (op // 100) % 10
-    # the second parameter's mode is in the thousands digit.
-    mode2 = (op // 1000) % 10
-    # the third parameter's mode is in the ten-thousands digit. 
-    mode3 = (op // 10000) % 10
-    return [mode1, mode2, mode3]
+def _decode_opcode(num):
+    digits = [0] * 5
+    pos = len(digits) - 1
+    while num > 0 and pos >= 0:
+        digits[pos] = num % 10
+        num //= 10
+        pos -= 1
+    return digits
 
-position_mode = 0
-intermediate_mode = 1
-def run(d):
+def _get_op1(program, pc, mode):
+    return program[program[pc + 1]] if mode == 0 else program[pc + 1]
+
+def _get_op2(program, pc, mode):
+    return program[program[pc + 2]] if mode == 0 else program[pc + 2]
+
+def run(program, input_id):
+    diagnostic_code = 0
     pc = 0
-    output = [0]
-    success = True
-    while pc < len(d):
-        opsize = 4
-        op = d[pc]
-        [mode1, mode2, mode3] = get_parameter_modes(op)
-        op = op % 100
-        if op == 99: # halt
-            opsize = 1
-            return [success, output[-1]]
-        elif op == 1: # add
-            # opsize = 4
-            [addr1, addr2, outAddr] = d[pc+1:pc+4]
-            v1 = addr1 if mode1 == position_mode else d[addr1]
-            v2 = addr2 if mode2 == position_mode else d[addr2]
-            v3 = outAddr if mode3 == position_mode else d[outAddr]
-            d[v3] = v1 + v2
-        elif op == 2: # multiply
-            # opsize = 4
-            [addr1, addr2, outAddr] = d[pc+1:pc+4]
-            v1 = 0
-            v2 = 0
-            v3 = 0
-            if mode1 == position_mode:
-                v1 = d[addr1]
-            else:
-                v1 = addr1
-            if mode2 == position_mode:
-                v2 = d[addr2]
-            else:
-                v2 = addr2
-            v3 = outAddr if mode3 == position_mode else d[outAddr]
-            d[v3] = v1 * v2
-        elif op == 3: # store
-            addr1 = d[pc+1]
-            v1 = addr1 if mode1 == 0 else d[addr1]
-            opsize = 2  # op + address
-            d[v1] = 1
-        elif op == 4: #retrive
-            addr1 = d[pc+1]
-            v1 = addr1 if mode1 == 0 else d[addr1]
-            opsize = 2
-            val = d[v1]
-            output.append(val)
-            if val != 0:
-                print(f"failure! {val}")
-                success = False
-            else:
-                print(val)
+    while pc < len(program):
+        opcode_obj = _decode_opcode(program[pc])
+        opcode = opcode_obj[3] * 10 + opcode_obj[4]
+        op1_mode = opcode_obj[2]
+        op2_mode = opcode_obj[1]
+        if opcode == 1 or opcode == 2:
+            op1, op2 = _get_op1(program, pc, op1_mode), _get_op2(program, pc, op2_mode)
+            dest = program[pc + 3]
+            program[dest] = op1 + op2 if opcode == 1 else op1 * op2
+            pc += 4
+        elif opcode == 3:
+            program[program[pc + 1]] = input_id
+            pc += 2
+        elif opcode == 4:
+            op1 = _get_op1(program, pc, op1_mode)
+            diagnostic_code = op1
+            pc += 2
+        elif opcode == 5 or opcode == 6:
+            op1, op2 = _get_op1(program, pc, op1_mode), _get_op2(program, pc, op2_mode)
+            pc = op2 if (opcode == 5 and op1 != 0) or (opcode == 6 and op1 == 0) else pc + 3
+        elif opcode == 7:
+            op1, op2 = _get_op1(program, pc, op1_mode), _get_op2(program, pc, op2_mode)
+            program[program[pc + 3]] = 1 if op1 < op2 else 0
+        elif opcode == 8:
+            op1, op2 = _get_op1(program, pc, op1_mode), _get_op2(program, pc, op2_mode)
+            program[program[pc + 3]] = 1 if op1 == op2 else 0
+            pc += 4
+        elif opcode == 99:
+            break
         else:
-            print(f"unknown op: {op} at pc: {pc}")
-            return [False, 999]
-        pc += opsize
-    return 0
+            print("unknown opcode.")
+            break
+    return diagnostic_code
 
-r = run(data)
-print(r)
+# part 1
+print(run(read_intcode("data.txt"),1))
