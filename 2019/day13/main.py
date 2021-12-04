30807593e05f50179d6b15134f01f449c71dae0a
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-04 01:07:06 +0100

    Fixes part 2

--- 2019/day13/main.py
+++ 2019/day13/main.py
@@ -21,3 +21,30 @@ def get_num_blocks(program: list) -> int:
 data = read_program("data.txt")
 r = get_num_blocks(data)
 print(r)
+
+def move_joystick(vm: Intcode, paddle_x: int, ball_x: int):
+    vm.set_input(-1 if paddle_x > ball_x else 1 if paddle_x < ball_x else 0)
+
+def beat_game(vm: Intcode) -> int:
+    score = 0
+    paddle_x = 0
+    while True:
+        x = vm.run()
+        if x is None:
+            break
+        y = vm.run()
+        data = vm.run()
+        if x == -1 and y == 0:
+            score = data
+        else:
+            if data == 3: # paddle
+                paddle_x = x
+            if data == 4: # ball 
+                move_joystick(vm, paddle_x, x)
+    return score
+
+# part 2
+program = read_program("data.txt")
+program[0] = 2 # play game
+r = beat_game(Intcode(program))
+print(r)
