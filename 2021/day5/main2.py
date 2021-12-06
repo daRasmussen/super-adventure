5bbbee742f2b1b59ec5be1bcf7512e1eb08b6437
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-06 14:11:53 +0100

    fixes part 2

--- 2021/day5/main2.py
+++ 2021/day5/main2.py
@@ -8,17 +8,19 @@ for line in open(infile):
     x2 = int(x2.strip())
     y1 = int(y1.strip())
     y2 = int(y2.strip())
-    x1, x2 = min(x1, x2), max(x1, x2)
-    y1, y2 = min(y1, y2), max(y1, y2)
-    if x1 == x2 or y1 == y2:
-        for x in range(x1, x2+1):
-            for y in range(y1, y2+1):
-                if (x,y) not in G:
-                    G[(x, y)] = 0
-                G[(x, y)] += 1
+    dx = x2-x1
+    dy = y2-y1
+    for i in range(1 + max(abs(dx), abs(dy))):
+        x = x1 + (1 if dx>0 else (-1 if dx<0 else 0)) * i
+        y = y1 + (1 if dy>0 else (-1 if dy<0 else 0)) * i
+        if (x,y) not in G:
+            G[(x,y)] = 0
+        G[(x,y)] += 1
+
 ans = 0
 for k in G:
     if G[k] > 1:
         ans += 1
 print('\n')
 print(ans)
+
