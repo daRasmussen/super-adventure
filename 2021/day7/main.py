366f10d91d630f0ab83399f70a58662124e053ae
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-07 14:03:55 +0100

    fixes part 2

--- 2021/day7/main.py
+++ 2021/day7/main.py
@@ -4,8 +4,14 @@ data = get_data(day=7, year=2021)
 
 X = [int(x) for x in data.split(",")]
 X.sort()
-ans = 0
-med = X[len(X)//2]
-for x in X:
-    ans += abs(x-med)
-submit(ans, part="a", day=7, year=2021)
+def C2(d):
+    return d*(d+1)/2
+best = 1e9
+for med in range(2000):
+    score = 0
+    for x in X:
+        d = abs(x-med)
+        score += C2(d)
+    if score < best:
+        best = score
+submit(int(best), part="b", day=7, year=2021)
