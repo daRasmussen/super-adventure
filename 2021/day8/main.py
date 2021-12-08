b460a2b84ca768328c7c742c79e0da6fed1368c9
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-08 14:13:52 +0100

    fixes part 2

--- 2021/day8/main.py
+++ 2021/day8/main.py
@@ -1,5 +1,6 @@
 from aocd import get_data, submit
 from collections import defaultdict
+import itertools
 
 data = get_data(day=8, year=2021).split("\n")
 ans = 0
@@ -7,10 +8,42 @@ for line in data:
     before, after = line.split("|")
     before = before.split()
     after = after.split()
-    by_len = defaultdict(list)
-    for w in before:
-        by_len[len(w)].append(w)
-    for w in after:
-        if len(by_len[len(w)]) == 1:
-            ans += 1
-submit(ans, part="a", day=8, year=2021)
+
+    digits = {
+        0: "abcefg",
+        1: "cf",
+        2: "acdeg",
+        3: "acdfg",
+        4: "bcdf",
+        5: "abdfg",
+        6: "abdefg",
+        7: "acf",
+        8: "abcdefg",
+        9: "abcdfg"
+    }
+    for perm in itertools.permutations(list(range(8))):
+        ok = True
+        D = {}
+        for i in range(8):
+            D[chr(ord("a")+i)] = chr(ord("a")+perm[i])
+        for w in before:
+            w_perm = ""
+            for c in w:
+                w_perm += D[c]
+            w_perm = "".join(sorted(w_perm))
+            if w_perm not in digits.values():
+                ok = False
+        if ok:
+            ret = ""
+            print("GOT")
+            for w in after:
+                w_perm = ""
+                for c in w:
+                    w_perm += D[c]
+                w_perm = "".join(sorted(w_perm))
+                d = [k for k,v in digits.items() if v == w_perm]
+                ret += str(d[0])
+            print(ret)
+            ans += int(ret)
+submit(ans, part="b", day=8, year=2021)
+
