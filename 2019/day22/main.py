6cfd873c0a018616d0ea5fc2a6a4df8fdcc22d88
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-10 01:41:18 +0100

    fixes part 2

--- 2019/day22/main.py
+++ 2019/day22/main.py
@@ -21,6 +21,26 @@ def track_pos(i):
             i = new_stack_i(i)
     return i
 ans = track_pos(2019)
-submit(ans, part='a', day=22, year=2019)
+# submit(ans, part='a', day=22, year=2019)
 
-# submit(ans, part='b', day=22, year=2019)
+N, REP = 119315717514047, 101741582076661
+def egcd(a, b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q,r = b//a,b%a; m,n = x-u*q,y-v*q 
+        b,a, x,y, u,v = a,r, u,v, m,n
+    return b, x, y
+
+def modinv(a, m):
+    g, x, y = egcd(a, m) 
+    return (None if g!=1 else x%m)
+
+p0, p1 = track_pos(0), track_pos(1)
+a1, b1 = (p1-p0)%N, p0
+a_t = modinv(a1, N) 
+b_t = (-a_t*b1)%N
+
+poly = lambda x: pow(a_t, REP, N)*x + (pow(a_t, REP, N)-1) * modinv(a_t-1, N) * b_t
+
+ans = poly(2020)%N
+submit(ans, part='b', day=22, year=2019)
