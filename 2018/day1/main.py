94d9746c13bf544324b75ef811b0319593c33ebd
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-11 20:22:20 +0100

    adds tests and fixes pyright config

--- 2018/day1/main.py
+++ 2018/day1/main.py
@@ -1,13 +1,31 @@
 from aocd import submit, get_data
 
+import pytest
+
+from collections import namedtuple
+
 data = get_data(day=1, year=2018)
-data = [int(x) for x in data.split("\n")]
+def part1(data: str) -> int:
+    return sum([int(x) for x in data.splitlines()])
 
-ans = 0
-for i in data:
-    ans += i
+TestData = namedtuple("TestData", ["data", "result"])
+TestData.__test__ = False # type: ignore
 
+@pytest.mark.parametrize(
+    ("data", "result"),
+    (
+      TestData("+1\n+1\n+1\n", 3),
+      TestData("+1\n+1\n-2\n", 0),
+      TestData("-1\n-2\n-3\n", -6),
+    )
+)
+def test(data, result):
+    assert part1(data) == result
+
+ans = part1(data)
 print(ans)
+#submit(ans, part='a', day=1, year=2018)
 
-submit(ans, part='a', day=1, year=2018)
-# submit(ans, part='b', day=1, year=2018)
+
+#submit(ans, part='b', day=1, year=2018)
+
