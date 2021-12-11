d4ad5d28339adc19b093cfb8f418f2570752b26d
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-11 14:33:28 +0100

    major hack, could not solve with py used old node version

--- 2019/day25/main.py
+++ 2019/day25/main.py
@@ -1,176 +1,205 @@
 from aocd import submit, get_data
 
 data = get_data(day=25, year=2019).split(",")
-
-strInputs = [int(x) for x in data]
-
-def getInputs(program, index, relative_base):
-  def handleMode(mode, param, relative_base):
-    if mode == 0:
-      return program[param]
-    if mode == 1:
-      return param
-    if mode == 2:
-      return program[param + relative_base]
-    return 0
-
-  opcode = program[index]
-  # 99 / 3 opcodes don't care about immediate v position mode
-  if (opcode == 99):
-    return (opcode, None, None, None)
-
-  # if (opcode == 3):
-  #   param1 = program[index + 1]
-  #   return (opcode, param1, None, None)
-
-  opcode = str(opcode)
-
-  if (len(opcode) < 3):
-    # opcode < 3 means no modes given, all should be 0
-    parsedOpcode = int(opcode)
-    modes = '000'
-  else:
-    # opcode is last 2 digits
-    parsedOpcode = int(opcode[-2:])
-    if (len(opcode) == 3):
-      modes = opcode[0] + '01'
-    if (len(opcode) == 4):
-      # grab all but last 2
-      modes = opcode[:-2]
-      # reverse digits so they're in order of params left
-      modes = modes[::-1]
-      modes += '1'
-    if (len(opcode) == 5):
-      # grab all but last 2
-      modes = opcode[:-2]
-      # reverse digits so they're in order of params left
-      modes = modes[::-1]
-
-
-  if (parsedOpcode < 3 or parsedOpcode == 7 or parsedOpcode == 8):
-    param1 = program[index + 1]
-    param2 = program[index + 2]
-    param3 = program[index + 3]
-    param1 = handleMode(int(modes[0]), param1, relative_base)
-    param2 = handleMode(int(modes[1]), param2, relative_base)
-    if int(modes[2]) == 2:
-      param3 += relative_base
-    return (parsedOpcode, param1, param2, param3)
-
-  if (parsedOpcode == 5 or parsedOpcode == 6):
-    param1 = program[index + 1]
-    param2 = program[index + 2]
-    param1 = handleMode(int(modes[0]), param1, relative_base)
-    param2 = handleMode(int(modes[1]), param2, relative_base)
-    return (parsedOpcode, param1, param2, None)
-  if (parsedOpcode == 4 or parsedOpcode == 9):
-    param1 = program[index + 1]
-    param1 = handleMode(int(modes[0]), param1, relative_base)
-    return (parsedOpcode, param1, None, None)
-  if (parsedOpcode == 3):
-    param1 = program[index + 1]
-    if int(modes[0]) == 2:
-      param1 += relative_base
-    # param1 = handleMode(int(modes[0]), param1, relative_base)
-    return (parsedOpcode, param1, None, None)
-
-def intcode(program, inputMethod, outputMethod = print, index = 0):
-  relative_base = 0
-  while index >= 0:
-    # print(index)
-    (opcode, param1, param2, param3) = getInputs(program, index, relative_base)
-    # print(f'opcode {opcode}, p1 {param1} p2 {param2} p3 {param3}')
-    # print(program)
-    if opcode == 99:
-      index = -1
-      return 'done'
-
-    # add
-    if opcode == 1:
-      program[param3] = param1 + param2
-      index += 4
-    # multiply
-    if opcode == 2:
-      program[param3] = param1 * param2
-      index += 4
-    #write input
-    if opcode == 3:
-      # take the input and save it to position
-      inputValue = inputMethod()
-      # print(f'got input {inputValue}, param {param1}, index {index}')
-      program[param1] = inputValue
-      index += 2
-    # output
-    if opcode == 4:
-      # print(f'outputting {param1}')
-      # take parameter and output it
-      outputMethod(param1)
-      # do whatever with output
-      index += 2
-    # jump-if-true
-    if opcode == 5:
-      index = param2 if param1 != 0 else index + 3
-    # jump-if-false
-    if opcode == 6:
-      index = param2 if param1 == 0 else index + 3
-    # less than
-    if opcode == 7:
-      program[param3] = 1 if param1 < param2 else 0
-      index += 4
-    # equals
-    if opcode == 8:
-      program[param3] = 1 if param1 == param2 else 0
-      index += 4
-    if opcode == 9:
-      relative_base += param1
-      index += 2
-
-inputs = list(map(lambda str: int(str), strInputs))
-for _ in range(100000):
-  inputs.append(0)
-
-def run_CYA_game(manual_mode = False):
-  output_string = ""
-  user_input = []
-  script_input = []
-
-  with open('./commands.script') as _file:
-    commands = _file.read().splitlines()
-
-  for line in commands:
-    if line != "" and line[0] != '#':
-      script_input.append(line)
-  script_input.reverse()
-
-  def inputter():
-    return user_input.pop()
-
-  def outputter(ascii_value):
-    nonlocal user_input, output_string
-    if ascii_value == 10:
-      print(output_string)
-      if output_string == 'Command?':
-        if manual_mode or len(script_input) == 0:
-          _user_input = input('\n')
-        else:
-          _user_input = script_input.pop()
-        _user_input = _user_input.strip().lower()
-        _user_input = [ord(char) for char in list(_user_input)]
-        _user_input.append(10)
-        _user_input.reverse()
-        user_input = _user_input
-      output_string = ""
-    else:
-      char = chr(ascii_value)
-      output_string += char
-
-  intcode(inputs, inputter, outputter)
-
-run_CYA_game(manual_mode=True)
-
+OP  = [int(x) for x in data]
+print("\n")
 
 ans = ''
 # submit(ans, part='a', day=25, year=2019)
 # submit(ans, part='b', day=25, year=2019)
 
+from collections import defaultdict, deque
+import sys
 
+class Program(object):
+    def __init__(self, pid, data, input):
+        self.P = defaultdict(int)
+        for i, x in enumerate(data):
+            self.P[i] = int(x)
+        self.input = input
+        self.ip = 0
+        self.pid = pid
+        self.rel_base = 0
+        self.halted = False
+    def idx(self, i, I):
+        mode = (0 if i >= len(I) else I[i])
+        val = self.P[self.ip+1+i]
+        if mode == 0:
+            pass # no - op
+        elif mode == 2:
+            val = val + self.rel_base
+        else:
+            assert False, mode
+        return val
+    def val(self, i, I):
+        mode = (0 if i >= len(I) else I[i])
+        val = self.P[self.ip+1+i]
+        if mode == 0:
+            val = self.P[val]
+        elif mode == 2:
+            val = self.P[val+self.rel_base]
+            print('val: ', val, "rel_base: ", self.rel_base)
+        return val
+    def run_all(self):
+        ans = []
+        while True:
+            val = self.run()
+            if val == None:
+                return ans
+            ans.append(val)
+    def run(self):
+        """ Return next output  """
+        while True:
+            cmd = str(self.P[self.ip])
+            opcode = int(cmd[-2:])
+            I = list(reversed([int(x) for x in cmd[:-2]]))
+            # print(opcode, I, self.P[self.ip:self.ip+4])
+            if opcode == 1:
+                # i1, i2 = self.val(0, I), self.val(1, I)
+                #while len(self.P) <= self.idx(2, I):
+                #    self.P.append(0)
+                self.P[self.idx(2, I)] = self.val(0, I) + self.val(1, I)
+                self.ip += 4
+            elif opcode == 2:
+                # i1, i2 = self.val(0,I), self.val(1, I)
+                #while len(self.P) <= self.idx(2, I):
+                #    self.P.append(0)
+                self.P[self.idx(2, I)] = self.val(0, I) * self.val(1, I)
+                self.ip += 4 
+            elif opcode == 3:
+                #while len(self.P) <= self.idx(0, I):
+                #    self.P.append(0)
+                self.P[self.idx(0, I)] = self.input
+                #self.Q.pop(0)
+                self.ip += 2
+            elif opcode == 4:
+                ans = self.val(0, I)
+                self.ip += 2
+                return ans
+            elif opcode == 5:
+                self.ip = self.val(1, I) if self.val(0, I) != 0 else self.ip + 3
+            elif opcode == 6:
+                self.ip = self.val(1, I) if self.val(0, I) == 0 else self.ip + 3
+            elif opcode == 7:
+                #while len(self.P) <= self.idx(2, I):
+                #    self.P.append(0)
+                print('TEEEST: :::::: ', self.val(0, I))
+                self.P[self.idx(2, I)] = (1 if self.val(0,I) < self.val(1, I) else 0)
+                self.ip += 4
+            elif opcode == 8:
+                #while len(self.P) <= self.idx(2, I):
+                #    self.P.append(0)
+                self.P[self.idx(2, I)] = (1 if self.val(0, I) == self.val(1, I) else 0)
+                self.ip += 4
+            elif opcode == 9:
+                self.rel_base += self.val(0, I)
+                self.ip += 2
+            else:
+                assert opcode == 99, opcode
+                self.halted = True
+                return None
+
+
+Q = deque()
+def get_input():
+    if Q:
+        return Q.popleft()
+    cmd = input()
+    for c in cmd:
+        Q.append(ord(c))
+    Q.append(ord("\n"))
+    return get_input()
+
+P = Program("0", data, get_input)
+while not P.halted:
+    o = int(P.run() or 0)
+    if P.halted:
+        break
+    if o <= 255:
+        sys.stdout.write(chr(o))
+    else:
+        sys.stdout.write(str(o))
+
+
+
+#CMDS = [
+#        "south",
+#        "take cake",
+#        "south",
+#        "west",
+#        "take mutex",
+#        "east",
+#        "north",
+#        "north",
+#        "west",
+#        "take klien bottle",
+#        "south",
+#        "east",
+#        "take monolith",
+#        "south",
+#        "take fuel cell",
+#        "west",
+#        "west",
+#        "take astrolabe",
+#        "east",
+#        "east",
+#        "north",
+#        "west",
+#        "north",
+#        "west",
+#        "north",
+#        "take tambourine",
+#        "south",
+#        "west",
+#        "take dark matter",
+#        "west"
+#]
+#items = [
+#    "mutex",
+#    "dark matter",
+#    "klein bottle",
+#    "tambourine",
+#    "fuel cell",
+#    "astrolabe",
+#    "monolith",
+#    "cake",
+#]
+#for item in items:
+#    CMDS.append(f"drop {item}")
+#for a in range(2**len(items)):
+#    iset = set()
+#    for i in range(len(items)):
+#        if ((a>>i)%1) == 1:
+#            iset.add(items[i])
+#    print(a, iset)
+#    for item in iset:
+#        CMDS.append(f"take {item}")
+#    CMDS.append("north")
+#    for item in iset:
+#        CMDS.append(f"drop {item}")
+#
+#cmds = []
+#Q = deque()
+#def get_input():
+#    if Q:
+#        return Q.popleft()
+#    if CMDS:
+#        cmd = CMDS.pop(0)
+#    else:
+#        cmd = input()
+#    if cmd == "QUIT":
+#        print(cmds)
+#        sys.exit(0)
+#    cmds.append(cmd)
+#    for c in cmd:
+#        Q.append(ord(c))
+#    Q.append(ord("\n"))
+#    return get_input()
+#
+#P = Program("0", OP, get_input)
+#while not P.halted:
+#    o = int(P.run() or 0)
+#    if P.halted:
+#        break
+#    if o <= 255:
+#        sys.stdout.write(chr(o))
