d556d80b4dac613fa6bdac47ea675b3bc1ac8167
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-12 06:52:05 +0100

    fixes part 2

--- 2021/day12/main.py
+++ 2021/day12/main.py
@@ -5,41 +5,19 @@ import pytest
 
 data = get_data(day=12, year=2021)
 print("\n")
-TestData = namedtuple("TestData", ["data", "result"])
-TestData.__test__ = False # type: ignore
 
-def get_data(filename):
-    with open(filename) as f:
-        return f.read()
-
-test1 = get_data("test1.txt")
-test2 = get_data("test2.txt")
-test3 = get_data("test3.txt")
-
-@pytest.mark.parametrize(
-    ("data", "result"),
-    (
-        TestData(test1, 10),
-        TestData(test2, 19),
-        TestData(test3, 226)
-    )
-)
-
-def test_part1(data, result):
-    assert part1(data) == result
-
-def part1(data):
+def part2(data):
     print(data)
     E = defaultdict(list)
     for line in data.splitlines():
         a, b = line.strip().split("-")
         E[a].append(b)
         E[b].append(a)
-    start = ("start", set(["start"]))
+    start = ("start", set(["start"]), None)
     Q = deque([start])
     ans = 0
     while Q:
-        pos, small = Q.popleft()
+        pos, small, twice = Q.popleft()
         if pos == "end":
             ans += 1
         for y in E[pos]:
@@ -47,12 +25,10 @@ def part1(data):
                 new_small = set(small)
                 if y.lower() == y:
                     new_small.add(y)
-                Q.append((y, new_small))
+                Q.append((y, new_small, twice))
+            elif y in small and twice is None and y not in ["start", "end"]:
+                Q.append((y, small, y))
     return ans
 
-ans = part1(data)
-print(ans)
-submit(ans, part='a', day=12, year=2021)
-
-ans = ""
-# submit(ans, part='b', day=12, year=2021)
+ans = part2(data)
+submit(ans, part='b', day=12, year=2021)
