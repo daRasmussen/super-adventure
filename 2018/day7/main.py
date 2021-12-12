1e707eebe998c7d1e863e1ae6b142837780ad6ac
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-12 17:50:29 +0100

    fixes part 2

--- 2018/day7/main.py
+++ 2018/day7/main.py
@@ -12,19 +12,35 @@ for line in data.splitlines():
     E[x].append(y)
     D[y] += 1
 
+for k in E:
+    E[k] = sorted(E[k])
+
+t = 0
+EV = []
 Q = []
+
+def add_task(x):
+    Q.append(x)
+def start_work():
+    global Q
+    while len(EV) < 5 and Q:
+        x = min(Q)
+        Q = [y for y in Q if y != x]
+        EV.append((t + 61 + ord(x) - ord("A"), x))
+
 for k in E:
     if D[k] == 0:
-        Q.append(k)
-ans = ""
-while Q:
-    x = sorted(Q)[0]
-    Q = [y for y in Q if y != x]
-    ans += x
+        add_task(k)
+start_work()
+
+while EV or Q:
+    t, x = min(EV)
+    EV = [y for y in EV if y!=(t,x)]
     for y in E[x]:
         D[y] -= 1
         if D[y] == 0:
-            Q.append(y)
+            add_task(y)
+    start_work()
+ans = t
 print(ans)
-submit(ans, part='a', day=7, year=2018)
-# submit(ans, part='b', day=7, year=2018)
+submit(ans, part='b', day=7, year=2018)
