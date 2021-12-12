be37dd7a96c676a7267c8f23a058e2ce6e104c7c
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-12 15:22:10 +0100

    fixes part 2

--- 2018/day5/main.py
+++ 2018/day5/main.py
@@ -2,19 +2,21 @@ from aocd import submit, get_data
 
 data = get_data(day=5, year=2018)
 
+alpha = "abcdefghijklmnopqrstuvwxyz"
+ans = 1e5
 M = {}
-for c in "abcdefghijklmnopqrstuvwxyz":
+for c in alpha:
     M[c.lower()] = c.upper()
     M[c.upper()] = c.lower()
 
-stack = []
-for c in data:
-    if stack and c == M[stack[-1]]:
-        stack.pop()
-    else:
-        stack.append(c)
-
-ans = len(stack)
+for rem in alpha:
+    s2 = [c for c in data if c!=rem.lower() and c!=rem.upper()]
+    stack = []
+    for c in s2:
+        if stack and c == M[stack[-1]]:
+            stack.pop()
+        else:
+            stack.append(c)
+    ans = min(ans, len(stack))
 print(ans)
-submit(ans, part='a', day=5, year=2018)
-# submit(ans, part='b', day=5, year=2018)
+submit(ans, part='b', day=5, year=2018)
