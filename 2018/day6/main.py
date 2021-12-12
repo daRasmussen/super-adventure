315b3fc39986296f85acd9b54120bbe716b51d22
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-12 17:26:08 +0100

    fixes part 2

--- 2018/day6/main.py
+++ 2018/day6/main.py
@@ -2,6 +2,7 @@ from aocd import submit, get_data
 import numpy as np
 import itertools
 import matplotlib.pyplot as plt
+#from matplotlib_terminal import plt
 from numpy.lib.arraysetops import unique
 
 data = get_data(day=6, year=2018)
@@ -27,13 +28,13 @@ perm = itertools.product(list(range(largest_coord)), repeat=2)
 distances = [get_shortest_distance(x) for x in perm]
 
 grid = np.array(distances).reshape(largest_coord, largest_coord)
-""" NOT WORKING  """
-#plt.figure(figsize=(12,8))
-#plt.title('Visualisation of areas')
-#plt.scatter(x_coords, y_coords, color='r', label='Coordinates')
-#plt.legend()
-#plt.imshow(grid.T)
-#plt.axis('off')
+
+plt.figure(figsize=(12,8))
+plt.title('Visualisation of areas')
+plt.scatter(x_coords, y_coords, color='r', label='Coordinates')
+plt.legend()
+plt.imshow(grid.T)
+plt.axis('off')
 
 edges = [grid[0], grid[max(x_coords)-1], grid[:,0], grid[:, max(y_coords)-1]]
 np.unique(edges)
@@ -42,5 +43,15 @@ unique, count = np.unique(grid, return_counts=True)
 inner_areas = [x for x in sorted(zip(count, unique)) if x[1] not in np.unique(edges)]
 ans, _ = inner_areas[-1]
 print(ans)
-submit(ans, part='a', day=6, year=2018)
-# submit(ans, part='b', day=6, year=2018)
+# submit(ans, part='a', day=6, year=2018)
+
+def is_point_under_total_distance(point, distance=10000):
+    distances = [abs(point[0] - coord[0]) + abs(point[1] - coord[1]) for coord in coords]
+    if sum(distances) < 10000:
+        return 1
+    return 0
+p = itertools.product(list(range(max(x_coords+y_coords))), repeat=2)
+under_10k = [is_point_under_total_distance(x) for x in p]
+ans = (sum(under_10k))
+print(ans)
+submit(ans, part='b', day=6, year=2018)
