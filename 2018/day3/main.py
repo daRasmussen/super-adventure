8ed03d79a1f1986e153e868989066790d5e2ec0a
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-12 07:24:45 +0100

    fixes part 2

--- 2018/day3/main.py
+++ 2018/day3/main.py
@@ -13,10 +13,18 @@ for line in data.splitlines():
     for dx in range(w):
         for dy in range(h):
             C[(x+dx, y+dy)] += 1
-ans = 0
-for (r, c),v in C.items():
-    if v >= 2:
-        ans += 1
-print(ans)
-submit(ans, part='a', day=3, year=2018)
-# submit(ans, part='b', day=3, year=2018)
+
+for line in data.splitlines():
+    words = line.split()
+    x, y = words[2].split(",")
+    x, y = int(x), int(y[:-1])
+    w, h = words[3].split("x")
+    w, h = int(w), int(h)
+    ok = True 
+    for dx in range(w):
+        for dy in range(h):
+            if C[(x+dx, y+dy)] > 1:
+                ok = False
+    if ok:
+        ans = words[0][1:]
+        submit(ans, part='b', day=3, year=2018)
