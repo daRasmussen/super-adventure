9890af0b607187279c5b962433b09592ec927b8b
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-13 11:49:22 +0100

    fixes part 2 in test.py output not working in main.py

--- 2021/day13/main.py
+++ 2021/day13/main.py
@@ -1,26 +1,39 @@
 from aocd import submit, get_data
-import itertools
-from collections import defaultdict, Counter, deque
 
 data = get_data(day=13, year=2021)
-ans = 0
 
 G = {}
 for line in data.splitlines():
     line = line.strip()
     if line and line.startswith("fold"):
         G2 = {}
-        for (x, y) in G:
-            if x < 655:
-                G2[(x, y)] = True
-            else:
-                G2[(655-(x-655), y)] = True
-        print(len(G2))
-        ans = len(G2)
+        instr = line.split()[-1]
+        d, v = instr.split("=")
+        v = int(v)
+        if d == "x":
+            for (x, y) in G:
+                if x < v:
+                    G2[(x, y)] = True
+                else:
+                    G2[(v-(x-v), y)] = True
+        else:
+            assert d == "y"
+            for (x, y) in G:
+                if y < v: 
+                    G2[(x, y)] = True
+                else:
+                    G2[x, (v-(y-v))] = True
+        G = G2
     elif line:
        x, y = [int(v) for v in line.strip().split(",")]
        G[(x, y)] = True
 
+X = max([x for x,_ in G.keys()])
+Y = max([y for _,y in G.keys()])
+
+ans = ""
+for y in range(Y+1):
+    for x in range(X+1):
+        ans += ("x" if (x, y) in G else " ")
 print(ans)
-submit(ans, part='a', day=13, year=2021)
-# submit(ans, part='b', day=13, year=2021)
+# output not working
