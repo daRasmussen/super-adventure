6fc8ebfd648bc9806b4011bc4f4e21e460eeaf5c
echo <echo>
2021-12-14 16:54:40 +0100

    clean up

--- utils/2015/day0/main.py
+++
@@ -1,9 +0,0 @@
-from aocd import submit, get_data
-
-data = get_data(day=0, year=2015)
-print(data)
-
-ans = ''
-
-# submit(ans, part='a', day=0, year=2015)
-# submit(ans, part='b', day=0, year=2015)
--- utils/2016/day0/main.py
+++
@@ -1,9 +0,0 @@
-from aocd import submit, get_data
-
-data = get_data(day=0, year=2016)
-print(data)
-
-ans = ''
-
-# submit(ans, part='a', day=0, year=2016)
-# submit(ans, part='b', day=0, year=2016)
