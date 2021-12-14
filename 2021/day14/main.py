97e8a5eda3b9a4137474ac46c6431dd8f0480615
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-14 12:31:44 +0100

    fixes part 2

--- 2021/day14/main.py
+++ 2021/day14/main.py
@@ -9,15 +9,22 @@ R = {}
 for rule in rules.split("\n"):
     start, end = rule.split(" -> ")
     R[start] = end
-for t in range(10):
-    T = ""
-    for i in range(len(S)):
-        T += S[i]
-        if i+1 < len(S):
-            T += R[S[i]+S[i+1]]
-    S = T
-C = Counter(S)
-ans = max(C.values())-min(C.values())
+C1 = Counter()
+for i in range(len(S)-1):
+    C1[S[i]+S[i+1]] += 1
+ans = 0
+for t in range(41):
+    if t in [10,40]:
+        CF = Counter()
+        for k in C1:
+            CF[k[0]] += C1[k]
+        CF[S[-1]] += 1
+        ans = max(CF.values())-min(CF.values())
+        print(ans)
+    C2 = Counter()
+    for k in C1:
+        C2[k[0]+R[k]] += C1[k]
+        C2[R[k]+k[1]] += C1[k]
+    C1 = C2
 print(ans)
-submit(ans, part='a', day=14, year=2021)
-# submit(ans, part='b', day=14, year=2021)
+submit(ans, part='b', day=14, year=2021)
