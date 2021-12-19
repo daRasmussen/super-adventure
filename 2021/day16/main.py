e2e950f398ba6e5e93a275f6eb349d95edbb7f46
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-19 08:20:38 +0100

    fixes part 2

--- 2021/day16/main.py
+++ 2021/day16/main.py
@@ -28,8 +28,44 @@ def ps1(startbit):
                 tv += v
 
     return i,tv
+### PART 2 ###
 
-ans = ps1(0)[1]
+import math
+
+op = [sum, math.prod, min, max,
+      lambda ls: ls[0], # literal
+      lambda ls: 1 if ls[0] > ls[1] else 0,  # gt
+      lambda ls: 1 if ls[0] < ls[1] else 0,  # lt
+      lambda ls: 1 if ls[0] == ls[1] else 0] # eq
+
+def ps2(startbit):
+    i = startbit # index into bs
+    ID = int(bs[i+3:i+6],2) # packet type ID
+    i += 6
+    if ID == 4: #literal value
+        vals = [0]
+        while True:
+            vals[0] = 16*vals[0] + int(bs[i+1:i+5],2)
+            i += 5
+            if bs[i-5] == '0': #last value packet
+                break
+    else:
+        vals = []
+        if bs[i] == '0': # subpacket length in bits
+            endi = i + 16 + int(bs[i+1:i+16],2)
+            i += 16
+            while i < endi:
+                i,v = ps2(i)
+                vals.append(v)
+        else:
+            np = int(bs[i+1:i+12],2) # number of subpackets
+            i += 12
+            for _ in range(np):
+                i,v = ps2(i)
+                vals.append(v)
+
+    return i,op[ID](vals)
+
+ans = ps2(0)[1]
 print(ans)
-submit(ans, part='a', day=16, year=2021)
-# submit(ans, part='b', day=16, year=2021)
+submit(ans, part='b', day=16, year=2021)
