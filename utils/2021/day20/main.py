d166aab23b54da0dd2fc143a914afc54295f0585
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-20 14:45:26 +0100

    fixes part 1

--- utils/2021/day20/main.py
+++
@@ -1,9 +0,0 @@
-from aocd import submit, get_data
-
-data = get_data(day=20, year=2021)
-print(data)
-
-ans = ''
-
-# submit(ans, part='a', day=20, year=2021)
-# submit(ans, part='b', day=20, year=2021)
