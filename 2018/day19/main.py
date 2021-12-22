63c3d7f65ff6a2a1a61d374f855e166514ba077b
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-22 23:35:53 +0100

    fixes part 2

--- 2018/day19/main.py
+++ 2018/day19/main.py
@@ -5,10 +5,10 @@ ans = ''
 
 lines = data.splitlines()
 a, b = int(lines[22].split()[2]), int(lines[24].split()[2])
-n = 836 + 22 * a + b
+n1 = 836 + 22 * a + b
+n = n1 + 10550400
 
 sqn = int(n ** .5)
 ans = sum(d + n // d for d in range(1, sqn + 1) if n % d == 0) - sqn * (sqn ** 2 == n)
 print(ans)
-submit(ans, part='a', day=19, year=2018)
-# submit(ans, part='b', day=19, year=2018)
+submit(ans, part='b', day=19, year=2018)
