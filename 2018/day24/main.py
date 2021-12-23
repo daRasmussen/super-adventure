c3a4e6353d03c3d1c1122e0b34c750f69909260a
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-23 01:02:25 +0100

    fixes part 2

--- 2018/day24/main.py
+++ 2018/day24/main.py
@@ -3,119 +3,142 @@ from aocd import submit, get_data
 data = get_data(day=24, year=2018)
 ans = ''
 
-from re import findall
+import re
+import itertools
+from copy import deepcopy
+from enum import auto, Enum
+from dataclasses import dataclass
+from typing import FrozenSet
 
-class Group:
- def __init__(self, rawCode, boost = 0):
-  numbers = [int(x) for x in findall(r'-?\d+', rawCode)]
-  self.units, self.hp, self.damage, self.initiative = numbers
-  self.damage   += boost
-  self.weakness  = []
-  self.immune    = []
-  self.defending = False
-  
-  if "weak" in rawCode:
-   weakS = rawCode.index("weak") + 8
-   if "immune" in rawCode and rawCode.index("immune") > rawCode.index("weak"):
-    weakE = rawCode.index(";")
-   else:
-    weakE = rawCode.index(")")
-   
-   weakStr = rawCode[weakS:weakE]
-   self.weakness = weakStr.split(", ")
-  
-  if "immune" in rawCode:
-   immuneS = rawCode.index("immune") + 10
-   if "weak" in rawCode and rawCode.index("immune") < rawCode.index("weak"):
-    immuneE = rawCode.index(";")
-   else: 
-    immuneE = rawCode.index(")")
-   immuneStr = rawCode[immuneS:immuneE]
-   self.immune = immuneStr.split(", ")
-  
-  words = rawCode.split()
-  
-  self.damageType = words[words.index("damage")-1]
- 
- def effectivePower(self):
-  return self.units * self.damage
+class Army(Enum):
+    IMMUNE_SYSTEM = auto()
+    INFECTION = auto()
 
-def calcDamage(attacker, defender):
- if attacker.damageType in defender.immune:
-  return 0
- elif attacker.damageType in defender.weakness:
-  return 2 * attacker.damage * attacker.units 
- else:
-  return attacker.damage * attacker.units 
+class Damage(Enum):
+    COLD = auto()
+    FIRE = auto()
+    SLASHING = auto()
+    RADIATION = auto()
+    BLUDGEONING = auto()
 
-def sortForDefend(attacker, groups):
- damageTaken = [calcDamage(attacker, defender) for defender in groups]
- effective   = [group.effectivePower() for group in groups]
- inits       = [group.initiative for group in groups]
- 
- return [group[3] for group in sorted(zip(damageTaken, effective, inits, groups), key = lambda k: (k[0], k[1], k[2]), reverse = True)]
+class Stalemate(Exception):
+    pass
 
-def sortForAttack(groups):
- effective = [group.effectivePower() for group in groups]
- inits     = [group.initiative for group in groups]
- 
- return [group[2] for group in sorted(zip(effective, inits, groups), key = lambda k: (k[0], k[1]), reverse = True)]
+@dataclass
+class Unit:
+    army: Army
+    count: int
+    hp: int
+    damage: int
+    attack: Damage
+    initiative: int
+    weaknesses: FrozenSet[Damage] = frozenset()
+    immunities: FrozenSet[Damage] = frozenset()
 
-def attack(attacker, defender):
- damage = calcDamage(attacker, defender)
- killed = min(defender.units, damage // defender.hp)
- defender.units = defender.units - killed
+    def __hash__(self): return id(self)  # allow using units in dictionaries
 
-def fight():
- pairs = []
- 
- for attackerGroups, defenderGroups in [(immuneGroups, infectGroups), (infectGroups, immuneGroups)]:
-  for attacker in sortForAttack(attackerGroups):
-   for defender in sortForDefend(attacker, defenderGroups):
-    if not defender.defending and calcDamage(attacker, defender):
-     defender.defending = True
-     pairs.append([attacker, defender])
-     break
-  
- pairs.sort(key = lambda k: (k[0].initiative), reverse = True)
- 
- return len([attack(*pair) for pair in pairs])
+    @classmethod
+    def parse(cls, army, val):
+        (count, hp, mods, damage, attack, initiative) = re.match(
+            r'(\d+) units each with (\d+) hit points(?: \((.*?)\))?'
+            r' with an attack that does (\d+) (\w+) damage at initiative (\d+)'
+        , val).groups()
 
-def cleanup():
- for groups in [immuneGroups, infectGroups]:
-  marked = []
-  for group in groups:
-   if not group.units:
-    marked.append(group)
-   else:
-    group.defending = False
+        kwargs = {}
 
-  for dead in marked:
-   groups.remove(dead)
-  
-def readFile():
-    content = data.splitlines()
-    return content
+        if mods:
+            for mod in mods.split('; '):
+                modifier, _, types = mod.split(' ', 2)
+                damages = frozenset(Damage[damage.upper()] for damage in types.split(', '))
+                if modifier == 'weak':
+                    kwargs['weaknesses'] = damages
+                elif modifier == 'immune':
+                    kwargs['immunities'] = damages
 
-inp = readFile()
+        return cls(army=army, count=int(count), hp=int(hp), damage=int(damage),
+            attack=Damage[attack.upper()], initiative=int(initiative), **kwargs)
 
-### Part 1
+    @property
+    def effective_power(self):
+        return self.count * self.damage
 
-immuneGroups, infectGroups = [], []
+    def damage_dealt(self, other):
+        if self.attack in other.immunities:
+            return 0
+        elif self.attack in other.weaknesses:
+            return self.effective_power * 2
+        else:
+            return self.effective_power
 
-for i in range(len(inp) // 2 - 1):
- immuneGroups.append(Group(inp[i+1]))
- infectGroups.append(Group(inp[i+13]))
+def round(armies):
+    targets = {}
+    attacking = {}
 
-while len(immuneGroups) and len(infectGroups):
- fight()
- cleanup()
+    for group in sorted(armies, key=lambda group: (group.effective_power, group.initiative), reverse=True):
+        if group.count <= 0:
+            continue
 
-result = 0
-for group in immuneGroups + infectGroups:
- result += group.units
- 
-ans = str(result)
-print(ans)
-submit(ans, part='a', day=24, year=2018)
-# submit(ans, part='b', day=24, year=2018)
+        enemies = [enemy for enemy in armies if enemy.army != group.army]
+        enemies = sorted(enemies, key=lambda enemy: (group.damage_dealt(enemy), enemy.effective_power, enemy.initiative), reverse=True)
+        target = next((enemy for enemy in enemies if enemy.count > 0 and group.damage_dealt(enemy) and enemy not in targets), None)
+
+        if not target:
+            continue
+
+        targets[target] = group
+        attacking[group] = target
+
+    stalemate = True
+
+    for group in sorted(armies, key=lambda group: group.initiative, reverse=True):
+        if group.count > 0 and attacking.get(group):
+            target = attacking[group]
+            killed = min(group.damage_dealt(target) // target.hp, target.count)
+
+            if killed:
+                target.count -= killed
+                stalemate = False
+
+    if stalemate:
+        raise Stalemate()
+
+    return armies
+
+def fight(armies, boost=0):
+    armies = deepcopy(armies)
+
+    for group in armies:
+        if group.army == Army.IMMUNE_SYSTEM:
+            group.damage += boost
+
+    while all(any(group.count for group in armies if group.army == army) for army in Army):
+        armies = round(armies)
+
+    return armies
+
+armies = []
+
+#for group in open('inputs/day24').read().split('\n\n'):
+for group in data.split("\n\n"):
+    name, *units = group.splitlines()
+
+    army = Army[name.replace(':', '').replace(' ', '_').upper()]
+
+    armies.extend(Unit.parse(army, line) for line in units)
+
+result = fight(armies)
+
+print('part 1:', sum(group.count for group in result if group.army == Army.INFECTION))
+
+for boost in itertools.count(1):
+    try:
+        result = fight(armies, boost=boost)
+    except Stalemate:
+        continue
+    else:
+        if all(group.count == 0 for group in result if group.army == Army.INFECTION):
+            break
+
+ans = sum(group.count for group in result if group.army == Army.IMMUNE_SYSTEM)
+print(ans) 
+submit(ans, part='b', day=24, year=2018)
