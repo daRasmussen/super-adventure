820e79212e47471979badf00cdd38f82de9f9abf
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-23 02:28:57 +0100

    fixes part 2

--- 2017/day1/main.py
+++ 2017/day1/main.py
@@ -3,21 +3,15 @@ from aocd import submit, get_data
 data = get_data(day=1, year=2017)
 ans = ''
 
-def digits(digitString):
-    index = 0
-    final = 0
-    while index < len(digitString):
-        ds1 = digitString[index]
-        ds2 = "-1"
-        if index == len(digitString) - 1:
-            ds2 = digitString[0]
-        else:
-            ds2 = digitString[index + 1]
-        if ds1 == ds2:
-            final += int(digitString[index])
-        index += 1
-    return final
-ans = digits(data)
+def compute(s: str) -> int:
+    s = s.strip()
+    total = 0
+    half = len(s) // 2
+    for i, c in enumerate(s):
+        if s[(i + half) % len(s)] == c:
+            total += int(c)
+    return total
+
+ans = compute(data)
 print(ans)
-submit(ans, part='a', day=1, year=2017)
-# submit(ans, part='b', day=1, year=2017)
+submit(ans, part='b', day=1, year=2017)
