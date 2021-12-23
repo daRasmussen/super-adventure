649e78bc183f9367ec615b377be06fad3be483cd
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-12-23 18:39:00 +0100

    fixes part 2

--- 2021/day23/main.py
+++ 2021/day23/main.py
@@ -5,17 +5,16 @@ ans = ''
 from rich import print
 from collections import deque
 
-R1T = 11
-R2T = 13
-R3T = 15
-R4T = 17
 COSTS = {"A": 1, "B": 10, "C": 100, "D": 1000}
 
 NO_STOP = {2, 4, 6, 8}
-TARGET = {"A": {11, 12}, "B": {13, 14}, "C": {15, 16}, "D": {17, 18}}
-TARGET_TOP = {"A": 11, "B": 13, "C": 15, "D": 17}
-START = "...........DCBACDAB"
-START_TEST = "...........BACDBCDA"
+TARGET = {
+    "A": {11, 12, 13, 14},
+    "B": {15, 16, 17, 18},
+    "C": {19, 20, 21, 22},
+    "D": {23, 24, 25, 26},
+}
+TARGET_TOP = {"A": 11, "B": 15, "C": 19, "D": 23}
 
 
 def switch(state, a, b):
@@ -24,33 +23,60 @@ def switch(state, a, b):
     return "".join(t)
 
 
-def moves(state):
+START = "..........." "DDDC" "BCBA" "CBAD" "AACB"
+START_TEST = "..........." "BDDA" "CCBD" "BBAC" "DACA"
+
+R1T = 11
+R2T = 15
+R3T = 19
+R4T = 23
+
+
+def moves2(state):
     # room top to room lower if target room and empty
     for rt in (R1T, R2T, R3T, R4T):
-        if state[rt] != "." and state[rt + 1] == "." and rt in TARGET[state[rt]]:
-            yield switch(state, rt, rt + 1), COSTS[state[rt]]
-            # no downside to move lower so we can avoid looking at other options
-            return
-    
-    # room lower to room top if empty and not target room
-    for rt in (R1T, R2T, R3T, R4T):
         if (
-            state[rt] == "."
-            and state[rt + 1] != "."
-            and rt not in TARGET[state[rt + 1]]
+            state[rt] != "."
+            and all(state[rt + n] in (".", state[rt]) for n in (1, 2, 3))
+            and rt in TARGET[state[rt]]
         ):
-            yield switch(state, rt, rt + 1), COSTS[state[rt + 1]]
-            # no downside to move up so we can avoid looking at other options
-            return
+            if state[rt + 1] == ".":
+                if state[rt + 2] == ".":
+                    if state[rt + 3] == ".":
+                        yield switch(state, rt, rt + 3), COSTS[state[rt]] * 3
+                        return
+                    else:
+                        yield switch(state, rt, rt + 2), COSTS[state[rt]] * 2
+                        return
+                else:
+                    yield switch(state, rt, rt + 1), COSTS[state[rt]]
+                    return
+
+    # room lower to room top if empty and not target room
+    for rt, target in ((R1T, "A"), (R2T, "B"), (R3T, "C"), (R4T, "D")):
+        if state[rt] == "." and any(
+            state[rt + n] not in (".", target) for n in (1, 2, 3)
+        ):
+            if state[rt + 1] != ".":
+                yield switch(state, rt, rt + 1), COSTS[state[rt + 1]]
+                return
+            elif state[rt + 2] != ".":
+                yield switch(state, rt, rt + 2), 2 * COSTS[state[rt + 2]]
+                return
+            else:
+                yield switch(state, rt, rt + 3), 3 * COSTS[state[rt + 3]]
+                return
 
     # hallway to room top if target and empty and room lower empty or has target and intervening empty
     for hall in range(11):
         if state[hall] == ".":
             continue
         target = TARGET_TOP[state[hall]]
-        if state[target] != "." or (state[target + 1] not in (".", state[hall])):
+        if state[target] != "." or any(
+            state[target + n] not in (".", state[hall]) for n in (1, 2, 3)
+        ):
             continue
-        h0 = target - 9
+        h0 = (target - 11) // 2 + 2
         if h0 > hall and all(state[h] == "." for h in range(hall + 1, h0)):
             yield switch(state, hall, target), COSTS[state[hall]] * (h0 - hall + 1)
             return
@@ -63,9 +89,11 @@ def moves(state):
         if state[rt] == ".":
             continue
         # Don't move out if right room and no blocker
-        if rt in TARGET[state[rt]] and state[rt + 1] in (".", state[rt]):
+        if rt in TARGET[state[rt]] and all(
+            state[rt + n] in (".", state[rt]) for n in (1, 2, 3)
+        ):
             continue
-        h0 = rt - 9
+        h0 = (rt - 11) // 2 + 2
         for hall in range(h0 - 1, -1, -1):
             if hall in NO_STOP:
                 continue
@@ -85,27 +113,29 @@ def show(state, cost):
         f"""\
         ############# {cost}
         #{state[:11]}#
-        ###{state[11]}#{state[13]}#{state[15]}#{state[17]}###
-          #{state[12]}#{state[14]}#{state[16]}#{state[18]}#
+        ###{state[11]}#{state[15]}#{state[19]}#{state[23]}###
+          #{state[12]}#{state[16]}#{state[20]}#{state[24]}#
+          #{state[13]}#{state[17]}#{state[21]}#{state[25]}#
+          #{state[14]}#{state[18]}#{state[22]}#{state[26]}#
           #########
 """
     )
 
 
-def part1(start: str, trace=False) -> int:
+def part2(start: str, trace=False) -> int:
     all_states = {start: (0, None)}
     queue = deque([start])
     while queue:
         state = queue.popleft()
         cost, _ = all_states[state]
         # print(state, cost)
-        for next, next_cost in moves(state):
+        for next, next_cost in moves2(state):
             if next in all_states and all_states[next][0] <= cost + next_cost:
                 continue
             all_states[next] = (cost + next_cost, state)
             queue.append(next)
 
-    st = "...........AABBCCDD"
+    st = "...........AAAABBBBCCCCDDDD"
     path = []
     while st:
         path.append((st, all_states[st][0]))
@@ -113,12 +143,11 @@ def part1(start: str, trace=False) -> int:
     if trace:
         for st, cost in path[::-1]:
             show(st, cost)
-    return all_states["...........AABBCCDD"][0]
+    return all_states["...........AAAABBBBCCCCDDDD"][0]
 
 
-assert part1(START_TEST) == 12521
-print("Part 1", part1(START, trace=True))
-ans = part1(START, trace=True )
+assert part2(START_TEST) == 44169
+print("Part 2", part2(START, True))
+ans = part2(START, True)
 print(ans)
-submit(ans, part='a', day=23, year=2021)
-# submit(ans, part='b', day=23, year=2021)
+submit(ans, part='b', day=23, year=2021)
