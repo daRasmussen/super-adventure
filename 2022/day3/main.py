03d6ececcd2948b4d1b258f907da6d2cf0060471
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2022-12-03 21:52:09 +0100

    fixes day3

--- 2022/day3/main.py
+++ 2022/day3/main.py
@@ -1,85 +1,28 @@
-with open('data.txt') as f:
-    bits = f.read().strip().split("\n")
-g = []
-e = []
-while len(g) < len(bits[0]):
-    t = []
-    for bit in bits:
-        target = bit[len(g)]
-        t.append(target)
-    o = [b for b in t if b == '1']
-    z = [b for b in t if b == '0']
-    g.append('0' if len(z) > len(o) else '1')
-    e.append('1' if len(z) > len(o) else '0')
-
-gamma = int("".join(g), 2)
-epsilon = int("".join(e), 2)
-power = gamma * epsilon
-print(power)
-
-# part 2
-test = [
-    "00100",
-    "11110",
-    "10110",
-    "10111",
-    "10101",
-    "01111",
-    "00111",
-    "11100",
-    "10000",
-    "11001",
-    "00010",
-    "01010",
-]
-def filter_ones(bits, index):
-    if len(bits) == 1:
-        return bits
+def score(c):
+    if 'a'<=c<='z':
+        return ord(c)-ord('a') + 1
     else:
-        ones, zeros= [], []
-        for bit in bits:
-            if bit[index] == '0':
-                zeros.append(bit)
-            if bit[index] == '1':
-                ones.append(bit)
-        index += 1
-        if len(ones) > len(zeros):
-            return filter_ones(ones, index)
-        elif len(ones) < len(zeros):
-            return filter_ones(zeros, index)
-        elif len(ones) == len(zeros):
-            return filter_ones(ones, index)
-def filter_zeros(bits, index):
-    if len(bits) == 1:
-        return bits
-    else:
-        ones, zeros= [], []
-        for bit in bits:
-            if bit[index] == '0':
-                zeros.append(bit)
-            if bit[index] == '1':
-                ones.append(bit)
-        index += 1
-        if len(ones) > len(zeros):
-            return filter_zeros(zeros, index)
-        elif len(ones) < len(zeros):
-            return filter_zeros(ones, index)
-        elif len(ones) == len(zeros):
-            return filter_zeros(zeros, index)
+        return ord(c)-ord('A') + 1 + 26
 
 
-o = filter_ones(test, 0)
-c = filter_zeros(test, 0)
-o = int(o[0], 2)
-c = int(c[0], 2)
-print(o, c, o * c)
+p1 = 0
+for line in open('data.txt'):
+    x = line.strip()
+    assert len(x)%2 == 0
+    y,z = x[:len(x)//2], x[len(x)//2:]
+    for c in y:
+        if c in z:
+            p1 += score(c)
+            break
+print(p1)
 
-ox = filter_ones(bits, 0)
-co = filter_zeros(bits, 0)
-ox = int(ox[0], 2)
-co = int(co[0], 2)
-life = ox * co
-print(life)
-        
-
-
+p2 = 0
+X = [line for line in open('data.txt')]
+i = 0
+while i < len(X):
+    for c in X[i]:
+        if c in X[i+1] and c in X[i+2]:
+            p2 += score(c)
+            break
+    i += 3
+print(p2)
