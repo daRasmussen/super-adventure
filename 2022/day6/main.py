433040e2ed0fac19a04107db4c46b6f2e812693b
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2022-12-06 11:05:34 +0100

    fixes day6

--- 2022/day6/main.py
+++ 2022/day6/main.py
@@ -1,21 +1,16 @@
-from collections import defaultdict
+#!/usr/bin/python3
+import sys
+from copy import deepcopy
+infile = sys.argv[1] if len(sys.argv)>1 else 'data.txt'
+data = open(infile).read()
+lines = [x for x in data.split('\n')]
 
-file = 'data.txt'
-N =[int(x) for x in open(file).read().strip().split(",")]
-
-X = defaultdict(int)
-for n in N:
-    if n not in X:
-        X[n] = 0
-    X[n] += 1
-
-for day in range(256):
-    Y = defaultdict(int)
-    for x,cnt in X.items():
-        if x == 0:
-            Y[6] += cnt
-            Y[8] += cnt
-        else:
-            Y[x-1] += cnt
-    X = Y
-print(sum(X.values()))
+p1 = False
+p2 = False
+for i in range(len(data)):
+    if (not p1) and i-3>=0 and len(set([data[i-j] for j in range(4)]))==4:
+        print(i+1)
+        p1 = True
+    if (not p2) and i-13>=0 and len(set([data[i-j] for j in range(14)]))==14:
+        print(i+1)
+        p2 = True
