092fe2e238997edf6cdfa9ec7236342846150c48
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2022-12-08 16:54:48 +0100

    fixes day7

--- 2022/day7/main.py
+++ 2022/day7/main.py
@@ -1,17 +1,41 @@
-from aocd import get_data, submit
+#!/usr/bin/python3
+import sys
+from collections import defaultdict
+infile = sys.argv[1] if len(sys.argv)>1 else 'data.txt'
+data = open(infile).read().strip()
+lines = [x for x in data.split('\n')]
 
-data = get_data(day=7, year=2021)
+# directory path -> total size of that directory (including subdirectories)
+SZ = defaultdict(int)
+path = []
+for line in lines:
+    words = line.strip().split()
+    if words[1] == 'cd':
+        if words[2] == '..':
+            path.pop()
+        else:
+            path.append(words[2])
+    elif words[1] == 'ls':
+        continue
+    elif words[0] == 'dir':
+        continue
+    else:
+        sz = int(words[0])
+        # Add this file's size to the current directory size *and* the size of all parents
+        for i in range(1, len(path)+1):
+            SZ['/'.join(path[:i])] += sz
 
-X = [int(x) for x in data.split(",")]
-X.sort()
-def C2(d):
-    return d*(d+1)/2
-best = 1e9
-for med in range(2000):
-    score = 0
-    for x in X:
-        d = abs(x-med)
-        score += C2(d)
-    if score < best:
-        best = score
-submit(int(best), part="b", day=7, year=2021)
+max_used = 70000000 - 30000000
+total_used = SZ['/']
+need_to_free = total_used - max_used
+
+p1 = 0
+p2 = 1e9
+for k,v in SZ.items():
+    #print(k,v)
+    if v <= 100000:
+        p1 += v
+    if v>=need_to_free:
+        p2 = min(p2, v)
+print(p1)
+print(p2)
